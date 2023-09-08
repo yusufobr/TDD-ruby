f5b9c8757fb4598a28f83c5c656cee955eff187f
yusufobr <yousarsif80@gmail.com>
2023-09-08 14:23:19 +0100

    fix linters erros

--- spec/solver_spec.rb
+++ spec/solver_spec.rb
@@ -1,28 +1,28 @@
 require_relative '../solver'
-describe Solver do 
-    before :each do
-        @solver = Solver.new
-    end 
-    describe '#solver' do
-        it 'returns an instance of solver' do
-            expect(@solver).to be_an_instance_of(Solver)
-        end
+describe Solver do
+  before :each do
+    @solver = Solver.new
+  end
+  describe '#solver' do
+    it 'returns an instance of solver' do
+      expect(@solver).to be_an_instance_of(Solver)
     end
-    
-    describe '#factorial' do
-        it 'returns 1 for input 0' do
-        result = @solver.factorial(0)
-        expect(result).to eq(1)
-        end
+  end
 
-        it 'returns 1 for input 1' do
-        result = @solver.factorial(1)
-        expect(result).to eq(1)
-        end
-
-        it 'returns the correct factorial for a positive number' do
-        result = @solver.factorial(5)
-        expect(result).to eq(120)
-        end
+  describe '#factorial' do
+    it 'returns 1 for input 0' do
+      result = @solver.factorial(0)
+      expect(result).to eq(1)
     end
+
+    it 'returns 1 for input 1' do
+      result = @solver.factorial(1)
+      expect(result).to eq(1)
+    end
+
+    it 'returns the correct factorial for a positive number' do
+      result = @solver.factorial(5)
+      expect(result).to eq(120)
+    end
+  end
 end

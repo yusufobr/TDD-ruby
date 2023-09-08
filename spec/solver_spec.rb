f2eeaeffa4b89e4138b99ddb2e9ac59877391546
yusufobr <yousarsif80@gmail.com>
2023-09-08 14:21:09 +0100

    add test for factorials

--- spec/solver_spec.rb
+++ spec/solver_spec.rb
@@ -5,7 +5,24 @@ describe Solver do
     end 
     describe '#solver' do
         it 'returns an instance of solver' do
-            @solver.should be_instance_of Solver
+            expect(@solver).to be_an_instance_of(Solver)
+        end
+    end
+    
+    describe '#factorial' do
+        it 'returns 1 for input 0' do
+        result = @solver.factorial(0)
+        expect(result).to eq(1)
+        end
+
+        it 'returns 1 for input 1' do
+        result = @solver.factorial(1)
+        expect(result).to eq(1)
+        end
+
+        it 'returns the correct factorial for a positive number' do
+        result = @solver.factorial(5)
+        expect(result).to eq(120)
         end
     end
 end

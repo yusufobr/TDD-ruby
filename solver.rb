684bab75e5de0eff8de20b61e496811e5ab6f373
yusufobr <yousarsif80@gmail.com>
2023-09-08 14:20:29 +0100

    add factorial class

--- solver.rb
+++ solver.rb
@@ -1,2 +1,10 @@
 class Solver
+    def factorial(num)
+        factorial = 1 # Initialize factorial to 1, not 0
+        while num > 0 do
+          factorial *= num
+          num -= 1 # Use -= instead of --
+        end
+        factorial # Return the factorial value
+      end
 end
